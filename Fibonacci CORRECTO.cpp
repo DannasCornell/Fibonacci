73ab35c93f81ac7033cb4029709fa719ba676e25
Marianaruizescutia <89041844+Marianaruizescutia@users.noreply.github.com>
2021-10-11 15:27:06 -0500

    Add files via upload

---
+++ Fibonacci CORRECTO.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int x,y,z,cont,stop;
+
+  x=0;
+  y=1;
+  z=0; 
+ 
+  printf("Cuantos numeros deseas: ");
+  scanf("%d", &stop);
+
+  //printf("0\n1\n");
+  //for(inicio; paro; incremento/decremento){}
+  
+  for (cont=0;cont<stop;cont++){
+  
+
+      //printf("%d\n",z);  //z = 1
+       
+      //z = 1 + 1= 2
+   	printf("%d\n",z);
+      x=y;  //x=1
+      y=z;  //y=2
+      z=x+y;
+     
+  }
+
+  getchar();
+  return 0;
+}
